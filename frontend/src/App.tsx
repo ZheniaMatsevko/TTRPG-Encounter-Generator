aa8b244f49014969d74d63661f2ce0da8e65cb0b
vanyatemno <vanya04400@gmail.com>
2025-03-14 10:05:55 +0200

    bugfix: text color for light/dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     return (
         <>
             <Router>
-                <div className="p-4 dark:bg-[#1E382A] text-[#FEF7EC] w-full h-screen">
+                <div className="p-4 dark:bg-[#1E382A] dark:text-[#FEF7EC] w-full h-screen">
                     <FilterContext.Provider value={{filters, setFilters}}>
                         <TopMenu/>
                         <Routes>
